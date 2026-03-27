3149d8500ad5e5df67c98e62b22b0503c6ee6188
R. Osinga <roaldosinga@gmail.com>
2026-03-27 14:38:20 +0100

    Fix: use ?file= query param in lesson link URL

--- lessons/00_setup.py
+++ lessons/00_setup.py
@@ -126,7 +126,7 @@ def _(Path, copy_all_btn, mo):
         import os
         _cwd = Path(os.getcwd())
         _first_file = Path(str(mo.notebook_location())).parent / "workspace" / "01_cells_and_reactivity.py"
-        _url = "/" + str(_first_file.relative_to(_cwd))
+        _url = "/?file=" + str(_first_file.relative_to(_cwd))
         _out = mo.callout(
             mo.md(
                 f"Copied {len(copy_all_btn.value)} lessons to `workspace/`.  \n\n"
